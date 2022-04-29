861f0300827f8aeb16eaed88a32191962b57cfb4
Shane Kelly <shane.kelly2408@gmail.com>
2022-04-29 16:18:58 +0100

    Fixed Firebase Auth with email and password

--- app.config.js
+++ app.config.js
@@ -1,4 +1,6 @@
-{
+import 'dotenv/config';
+
+export default {
   "expo": {
     "name": "artista1",
     "slug": "artista1",
@@ -27,6 +29,16 @@
     },
     "web": {
       "favicon": "./assets/favicon.png"
+    },
+    "extra": {
+      firebaseApiKey: process.env.FB_API_KEY,
+      firebaseAuthDomain: process.env.FB_AUTH_DOMAIN,
+      firebaseDatabaseUrl: process.env.FB_DB_URL,
+      firebaseProjectId: process.env.FB_PROJECT_ID,
+      firebaseStorageBucket: process.env.FB_STORAGE_BUCKET,
+      firebaseMessagingSenderId: process.env.FB_MESSAGING_SENDER_ID,
+      firebaseAppId: process.env.FB_APP_ID,
+      firebaseMeasurementId: process.env.FB_MEASUREMENT_ID
     }
   }
 }
