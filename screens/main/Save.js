ac0fe575e9aab98c7964587e8fc1600c8d860b8a
Alexkelly1 <68341974+Alexkelly1@users.noreply.github.com>
2022-05-16 13:26:22 +0200

    Local Changes

--- screens/main/Save.js
+++ screens/main/Save.js
@@ -6,7 +6,7 @@ import { getStorage, ref, uploadBytes } from "firebase/storage";
 const storage = getStorage();
 
 export const Save = (props) => {
-    const [caption, setCaption] = useState("");
+    const [caption, setCaption] = useState(Math.random().toString(36));
     const imagePath = props.route.params.image;
 
     const handleImageUpload = async () => {
@@ -19,51 +19,15 @@ export const Save = (props) => {
         uploadBytes(storageRef, conversion)
             .then(_ => console.log("Upload completed"))
             .catch(err => console.log(err));
-    };
+    };    
 
-import React, { useState } from 'react'
-import { View, TextInput, Image, Button } from 'react-native'
-
-import firebase from 'firebase/app'
-
-export default function Save(props) {
-    const [caption, setCaption] = useState("")
-
-    const uploadImage = async () => {
-        const uri = props.route.params.image;
-        const childPath = `post/${firebase.auth().currentUser.uid}/${Math.random().toString(36)}`
-        console.log(childPath)
-
-        const response = await fetch(uri)
-        const blob = await response.blob();
-
-        const task = firebase
-            .storage()
-            .ref()
-            .child(childPath)
-            .put(blob)
-        
-        const taskProgress = snapshot => {
-            console.log(`transferred: ${snapshot.bytesTransferred}`)
-        }
-        
-        const taskCompleted = () => {
-            task.snapshot.ref.getDownloadURL().then((snapshot) => {
-                console.log(snapshot)
-            })
-        }
-
-        const taskError = snapshot => {
-            console.log(snapshot)
-        }
-
-        task.on("state_changed", taskProgress, taskError, taskCompleted);
-    }
     return (
         <View style={{ flex: 1 }}>
             <Image source={{ uri: imagePath }} />
             <TextInput
+                style={{padding: 10}}
                 placeholder="Caption..."
+                placeholderTextColor={"#000000"}
                 onChangeText={(caption) => setCaption(caption)}
             />
             <Button title="Post" onPress={() => handleImageUpload()} />
